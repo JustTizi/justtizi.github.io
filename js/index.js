48f349f4140126cfb3f6992bb6cd2297ecdfc603
justtizi <vdwtizi@protonmail.com>
2023-02-19 17:51:47 +0100

    Homepage done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
+let menu = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navbar.classList.toggle('active')
+}
+
+const sr = ScrollReveal ({
+    distance: '80px',
+    duration: 2800,
+    reset: true
+})
+
+sr.reveal('.text, .social', {delay: 200, origin: 'left'})
+sr.reveal('header', {delay: 50, origin: 'top'})
+
 console.log("JavaScript succesfully loaded!");
